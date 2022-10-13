84094da8dfc6be521b356f55e3ae7edc9043d180
Matt Martz <matt.martz@gmail.com>
2022-10-13 16:55:06 -0400

    working edge auth with auth0

---
+++ lib/auth.ts
@@ -0,0 +1,258 @@
+import type {
+  CloudFrontHeaders,
+  CloudFrontRequest,
+  CloudFrontRequestCallback,
+  CloudFrontRequestEvent,
+  CloudFrontRequestResult,
+  CloudFrontResponse,
+} from "aws-lambda";
+import {
+  GetSecretValueCommand,
+  SecretsManagerClient,
+} from "@aws-sdk/client-secrets-manager";
+import { request as httpsRequest } from "https";
+import { parse, ParsedUrlQuery, stringify } from "querystring";
+import { verify } from "jsonwebtoken";
+import type { Algorithm } from "jsonwebtoken";
+const SecretId = "blog-cloudformation-edge/auth0";
+
+const sm = new SecretsManagerClient({});
+
+const PUBLIC_PATHS = [/\/favicons\//];
+
+const unauthorizedResponse = {
+  status: "401",
+  statusDescription: "Unauthorized",
+  body: `
+    <\!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8">
+        <title>Unauthorized</title>
+      </head>
+      <body>
+        <p>Unauthorized</p>
+      </body>
+    </html>
+    `,
+};
+
+export interface Auth0Creds {
+  certificate: string;
+  AUTH0_CLIENT_ID: string;
+  AUTH0_CLIENT_SECRET: string;
+  AUTH0_ALGORITHM: Algorithm;
+  AUTH0_DOMAIN: string;
+  AUTH0_HOST: string;
+  AUTH0_LOGIN_URL: string;
+  CALLBACK_PATH: string;
+}
+
+let auth0Creds: Auth0Creds;
+let distributionDomainName: string;
+
+const redirect = (
+  newLocation: string,
+  cookies: { name: string; value: string }[]
+): CloudFrontResponse => {
+  const result: CloudFrontResponse = {
+    status: "302",
+    statusDescription: "Found",
+    headers: {
+      location: [
+        {
+          key: "Location",
+          value: newLocation,
+        },
+      ],
+    },
+  };
+
+  if (cookies) {
+    result.headers["set-cookie"] = cookies.map((c) => ({
+      key: "set-cookie",
+      value: `${c.name}=${c.value}`,
+    }));
+  }
+
+  return result;
+};
+
+const loginCallback = async (
+  request: CloudFrontRequest
+): Promise<CloudFrontResponse | void> => {
+  console.log(`callback? ${request.uri} !== ${auth0Creds.CALLBACK_PATH}: ${request.uri !== auth0Creds.CALLBACK_PATH}`)
+  if (request.uri !== auth0Creds.CALLBACK_PATH) {
+    return;
+  }
+
+  let params: ParsedUrlQuery;
+  try {
+    params = parse(request.querystring);
+    if (params.error) {
+      console.log(params.error);
+      throw new Error("Unauthorized");
+    }
+    if (!params.code) {
+      return;
+    }
+  } catch (err) {
+    console.log(err);
+    return;
+  }
+
+  // Call Auth0 to get JWT token
+  const postData = stringify({
+    client_id: auth0Creds.AUTH0_CLIENT_ID,
+    redirect_uri: `https://${distributionDomainName}${auth0Creds.CALLBACK_PATH}`,
+    client_secret: auth0Creds.AUTH0_CLIENT_SECRET,
+    code: params.code,
+    grant_type: "authorization_code",
+  });
+  const postOptions = {
+    host: auth0Creds.AUTH0_DOMAIN,
+    port: 443,
+    path: "/oauth/token",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Content-Length": postData.length,
+    },
+  };
+  const tokenRequest: Promise<CloudFrontResponse> = new Promise((resolve) => {
+    const req = httpsRequest(postOptions, (res) => {
+      if (!res) {
+        throw new Error("No Res");
+      }
+      if (res.statusCode! >= 300) {
+        throw new Error("Bad Response");
+      }
+  
+      let body = "";
+      res.on("data", (d) => (body += d));
+      res.on("end", () => {
+        const json = JSON.parse(body);
+        const token = json.id_token;
+  
+        if (!token) {
+          throw new Error("Unauthorized");
+        }
+  
+        // store this in a cookie, then redirect the user
+        const dest = `https://${distributionDomainName}${params.dest || "/"}`;
+        return resolve(redirect(dest, [{ name: "session-token", value: token }]));
+      });
+    });
+    req.on("error", (e) => {
+      throw e;
+    });
+    req.write(postData);
+    req.end();
+  });
+  return tokenRequest;
+};
+
+const parseCookies = (headers: CloudFrontHeaders) => {
+  const parsedCookie: Record<string, string> = {};
+  if (headers.cookie) {
+    headers.cookie[0].value.split(";").forEach((cookie) => {
+      if (cookie) {
+        const parts = cookie.split("=");
+        parsedCookie[parts[0].trim()] = parts[1].trim();
+      }
+    });
+  }
+  return parsedCookie;
+};
+
+const validateToken = (token: string) => {
+  try {
+    verify(token, auth0Creds.certificate, {
+      algorithms: [auth0Creds.AUTH0_ALGORITHM],
+      audience: auth0Creds.AUTH0_CLIENT_ID,
+    });
+  } catch (e) {
+    return false;
+  }
+  return true;
+};
+
+const validateCookie = (cookie: string) => {
+  return !!cookie && validateToken(cookie);
+};
+
+const checkToken = (request: CloudFrontRequest) => {
+  const headers = request.headers;
+
+  /* Check for session-id in request cookie in viewer-request event,
+   * if session-id is absent, redirect the user to sign in page with original
+   * request sent as redirect_url in query params.
+   */
+
+  /* Check for session-id in cookie, if present then proceed with request */
+  const parsedCookies = parseCookies(headers);
+  if (validateCookie(parsedCookies["session-token"])) {
+    return; // not handled
+  }
+
+  // User is not authenticated.
+  /* URI encode the original request so we can send as query param for when user is finally logged in */
+  const encodedRedirectUrl = encodeURIComponent(
+    request.querystring ? `${request.uri}?${request.querystring}` : request.uri
+  );
+  const callbackUrl = `https://${distributionDomainName}${auth0Creds.CALLBACK_PATH}?dest=${encodedRedirectUrl}`;
+  const encodedCallback = encodeURIComponent(callbackUrl);
+  const redirectUrl = `${auth0Creds.AUTH0_LOGIN_URL}?client=${auth0Creds.AUTH0_CLIENT_ID}&redirect_uri=${encodedCallback}`;
+
+  return redirect(redirectUrl, [{ name: "session-token", value: "" }]);
+};
+
+const asyncHandler = async (
+  request: CloudFrontRequest
+): Promise<CloudFrontRequestResult> => {
+  if (!auth0Creds) {
+    const getSecret = new GetSecretValueCommand({
+      SecretId,
+    });
+    const secret = await sm.send(getSecret);
+    auth0Creds = JSON.parse(secret.SecretString!) as Auth0Creds;
+  }
+  const loggedIn = await loginCallback(request);
+  if (loggedIn) {
+    console.log('was login callback');
+    return loggedIn;
+  }
+  const redirectToLogin = checkToken(request);
+  if (redirectToLogin) {
+    console.log('not logged in');
+    return redirectToLogin;
+  }
+
+  console.log('authenticated!');
+  return request;
+};
+
+export const handler = (
+  event: CloudFrontRequestEvent,
+  _: any,
+  callback: CloudFrontRequestCallback
+) => {
+  const request = event.Records[0].cf.request;
+  distributionDomainName = event.Records[0].cf.config.distributionDomainName;
+
+  if (PUBLIC_PATHS.find((pattern) => pattern.test(request.uri))) {
+    callback(null, request);
+    return;
+  }
+
+  console.log(JSON.stringify(event, null, 2));
+  asyncHandler(request)
+    .then((res) => {
+      console.log(JSON.stringify(res, null, 2));
+      callback(null, res);
+    })
+    .catch((e) => {
+      console.log(e);
+      callback(null, unauthorizedResponse);
+    });
+};
--- lib/blog-cdk-cloudfront-edge-auth0-stack.ts
+++ lib/blog-cdk-cloudfront-edge-auth0-stack.ts
@@ -1,16 +1,75 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { CfnOutput, RemovalPolicy } from "aws-cdk-lib";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import { CacheHeaderBehavior, CachePolicy, CacheQueryStringBehavior } from "aws-cdk-lib/aws-cloudfront";
+import { S3Origin } from "aws-cdk-lib/aws-cloudfront-origins";
+import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { Code, Runtime } from "aws-cdk-lib/aws-lambda";
+import { RetentionDays } from "aws-cdk-lib/aws-logs";
+import { BlockPublicAccess, Bucket, ObjectOwnership } from "aws-cdk-lib/aws-s3";
+import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
+import { Secret } from "aws-cdk-lib/aws-secretsmanager";
+import { Construct } from "constructs";
+import { join } from "path";
 
 export class BlogCdkCloudfrontEdgeAuth0Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new Bucket(this, "Bucket", {
+      removalPolicy: RemovalPolicy.DESTROY,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      objectOwnership: ObjectOwnership.BUCKET_OWNER_ENFORCED,
+      autoDeleteObjects: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BlogCdkCloudfrontEdgeAuth0Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new BucketDeployment(this, "BucketDeployment", {
+      destinationBucket: bucket,
+      sources: [Source.asset(join(__dirname, "../dist"))],
+    });
+
+    const originAccessIdentity = new cloudfront.OriginAccessIdentity(
+      this,
+      "OriginAccessIdentity"
+    );
+    bucket.grantRead(originAccessIdentity);
+
+    const authFn = new cloudfront.experimental.EdgeFunction(
+      this,
+      `CloudFrontAuthFn`,
+      {
+        code: Code.fromAsset(join(__dirname, "./auth-lambda/")),
+        handler: "index.handler",
+        runtime: Runtime.NODEJS_16_X,
+        logRetention: RetentionDays.ONE_DAY,
+      }
+    );
+    const secret = Secret.fromSecretNameV2(
+      this,
+      "Auth0Secret",
+      "blog-cloudformation-edge/auth0"
+    );
+    secret.grantRead(authFn);
+
+    const cloudFront = new cloudfront.Distribution(this, "Distribution", {
+      defaultRootObject: "index.html",
+      defaultBehavior: {
+        origin: new S3Origin(bucket, { originAccessIdentity }),
+        cachePolicy:new CachePolicy(this, `CachePolicy`, {
+          cookieBehavior: cloudfront.CacheCookieBehavior.all(),
+          queryStringBehavior: CacheQueryStringBehavior.all(),
+        }),
+        edgeLambdas: [
+          {
+            functionVersion: authFn.currentVersion,
+            eventType: cloudfront.LambdaEdgeEventType.VIEWER_REQUEST,
+          },
+        ],
+      },
+    });
+
+    new CfnOutput(this, `CloudFrontUrl`, {
+      value: `https://${cloudFront.distributionDomainName}`,
+    });
   }
 }
